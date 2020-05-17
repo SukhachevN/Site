17a81a77225f99288ef3570ea4ca488a9f7b4e78
SukhachevN <43090884+SukhachevN@users.noreply.github.com>
2020-05-17 13:33:23 +0300

    Add files via upload
    Добавил все файлы

---
+++ minicms/menu.php
@@ -0,0 +1,11 @@
+<div class="headers-cont2">
+<div class="content"><img src="images/buttons/button_novosti.png" alt="news" class="space"><! -- невидимая картинка чтобы выровнять кнопки, надо исправить -->
+      <a href="?option=category&id_category=1"><img src="images/buttons/button_novosti.png" alt="news" class="buttons"></a><! -- ТУТ УКАЗАТЬ id_category из БАЗЫ ДАННЫХ -->
+      <a href="?option=category&id_category=2"><img src="images/buttons/button_galereya.png" alt="galery" class="buttons"></a>
+      <a href="?option=category&id_category=3"><img src="images/buttons/button_gajdy.png" alt="guides" class="buttons"></a>
+      <a href="?option=category&id_category=4"><img src="images/buttons/button_recepty.png" alt="recepts" class="buttons"></a>
+      <a href="?option=category&id_category=5"><img src="images/buttons/button_loshadi.png" alt="horses" class="buttons"></a>
+      <a href="?option=category&id_category=6"><img src="images/buttons/button_karty.png" alt="maps" class="buttons"></a>
+      <a href="?option=category&id_category=7"><img src="images/buttons/button_bossy.png"  alt="boss" class="buttons"></a></div>
+      </div>
+<div class="semilayer">
---
+++ minicms/rightbar.php
@@ -0,0 +1,118 @@
+<div class="smallmenu">
+            <center>
+               <h2>База предметов</h2>
+            </center>
+            <ul class="topmenu">
+               <li>
+                  <a href="">Материалы</a>
+                  <ul class="submenu">
+                     <li><a href="?option=category&id_category=8">Продукты</a></li><! -- ТУТ УКАЗАТЬ id_category из БАЗЫ ДАННЫХ -->
+                     <li><a href="?option=category&id_category=9">Мясо, морепродукты, охотничьи трофеи</a></li>
+                     <li><a href="?option=category&id_category=10">Реагенты</a></li>
+                     <li><a href="?option=category&id_category=11">Редкие ресурсы</a></li>
+                     <li><a href="?option=category&id_category=12">Порошки</a></li>
+                     <li><a href="?option=category&id_category=13">Грибы</a></li>
+                     <li><a href="?option=category&id_category=14">Травы</a></li>
+                     <li><a href="?option=category&id_category=15">Кораллы</a></li>
+                     <li><a href="?option=category&id_category=16">Кровь</a></li>
+                     <li><a href="?option=category&id_category=17">Кожа</a></li>
+                     <li><a href="?option=category&id_category=18">Руда</a></li>
+                     <li><a href="?option=category&id_category=19">Металлы</a></li>
+                     <li><a href="?option=category&id_category=20">Драгоценные камни</a></li>
+                     <li><a href="?option=category&id_category=21">Древесина</a></li>
+                     <li><a href="?option=category&id_category=22">Доски</a></li>
+                     <li><a href="?option=category&id_category=23">Ткани</a></li>
+                     <li><a href="?option=category&id_category=24">Перья</a></li>
+                  </ul>
+               </li>
+               <li><a href="?option=view&id_category=25">Зелья</a></li>
+               <li><a href="?option=view&id_category=26">Еда</a></li>
+               <li>
+                  <a href="?option=view&id_category=27">Разное</a>
+                  <ul class="submenu">
+                     <li><a href="?option=category&id_category=28">Без категории</a></li>
+                     <li><a href="?option=category&id_category=29">Алхимические камни</a></li>
+                     <li><a href="?option=category&id_category=30">Заточки и печати</a></li>
+                     <li><a href="?option=category&id_category=31">Инструменты</a></li>
+                     <li><a href="?option=category&id_category=32">Предметы для питомцев</a></li>
+                     <li><a href="?option=category&id_category=33">Предметы для огорода</a></li>
+                     <li><a href="?option=category&id_category=34">Семена</a></li>
+                  </ul>
+               </li>
+               <li>
+                  <a href="">Доспехи</a>
+                  <ul class="submenu">
+                     <li><a href="?option=category&id_category=35">Основные доспехи</a></li>
+                     <li><a href="?option=category&id_category=36">Шлемы</a></li>
+                     <li><a href="?option=category&id_category=37">Перчатки</a></li>
+                     <li><a href="?option=category&id_category=38">Обувь</a></li>
+                  </ul>
+               </li>
+               <li>
+                  <a href="">Оружие</a>
+                  <ul class="submenu">
+                     <li><a href="?option=category&id_category=39">Талисманы</a></li>
+                     <li><a href="?option=category&id_category=40">Амулеты</a></li>
+                     <li><a href="?option=category&id_category=41">Луки</a></li>
+                     <li><a href="?option=category&id_category=42">Кинжалы</a></li>
+                     <li><a href="?option=category&id_category=43">Топоры</a></li>
+                     <li><a href="?option=category&id_category=44">Темляки</a></li>
+                     <li><a href="?option=category&id_category=45">Мечи</a></li>
+                     <li><a href="?option=category&id_category=46">Щиты</a></li>
+                     <li><a href="?option=category&id_category=47">Клинки</a></li>
+                     <li><a href="?option=category&id_category=48">Брелки</a></li>
+                     <li><a href="?option=category&id_category=49">Посохи</a></li>
+                     <li><a href="?option=category&id_category=50">Кинжалы</a></li>
+                  </ul>
+               </li>
+               <li>
+                  <a href="">Бижутерия</a>
+                  <ul class="submenu">
+                     <li><a href="?option=category&id_category=51">Ожерелья</a></li>
+                     <li><a href="?option=category&id_category=52">Серьги</a></li>
+                     <li><a href="?option=category&id_category=53">Кольца</a></li>
+                     <li><a href="?option=category&id_category=54">Пояса</a></li>
+                  </ul>
+               </li>
+               <li>
+                  <a href="">Камни инкрустации</a>
+                  <ul class="submenu">
+                     <li><a href="?option=category&id_category=55">В шлем</a></li>
+                     <li><a href="?option=category&id_category=56">В доспех</a></li>
+                     <li><a href="?option=category&id_category=57">В перчатки</a></li>
+                     <li><a href="?option=category&id_category=58">В обувь</a></li>
+                     <li><a href="?option=category&id_category=59">В оружие</a></li>
+                     <li><a href="?option=category&id_category=60">В доп. оружие</a></li>
+                     <li><a href="?option=category&id_category=61">В любой слот</a></li>
+                  </ul>
+               </li>
+               <li>
+                  <a href="">Транспорт</a>
+                  <ul class="submenu">
+                     <li><a href="?option=category&id_category=62">Повозки</a></li>
+                     <li><a href="?option=category&id_category=63">Крыши</a></li>
+                     <li><a href="?option=category&id_category=64">Знамена</a></li>
+                     <li><a href="?option=category&id_category=65">Колеса</a></li>
+                     <li><a href="?option=category&id_category=66">Вывески</a></li>
+                     <li><a href="?option=category&id_category=67">Лодки</a></li>
+                     <li><a href="?option=category&id_category=68">Трюмы</a></li>
+                     <li><a href="?option=category&id_category=69">Такелаж</a></li>
+                     <li><a href="?option=category&id_category=70">Водорезы</a></li>
+                  </ul>
+               </li>
+               <li>
+                  <a href="">Торговые паки</a>
+                  <ul class="submenu">
+                     <li><a href="?option=category&id_category=71">С рудой и металлами</a></li>
+                     <li><a href="?option=category&id_category=72">С деревом</a></li>
+                  </ul>
+               </li>
+               <li>
+                  <a href="">Имперские паки</a>
+                  <ul class="submenu">
+                     <li><a href="?option=category&id_category=73">Кулинарные</a></li>
+                     <li><a href="?option=category&id_category=74">Алхимические</a></li>
+                  </ul>
+               </li>
+            </ul>
+         </div>
