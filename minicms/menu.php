34f4b29ea2cbcec86bb4248f0800a978f3194f87
SukhachevN <43090884+SukhachevN@users.noreply.github.com>
2020-05-18 14:28:25 +0300

    Add files via upload
    Изменение главной страницы

--- minicms/menu.php
+++ minicms/menu.php
@@ -1,11 +1,2 @@
 <div class="headers-cont2">
-<div class="content"><img src="images/buttons/button_novosti.png" alt="news" class="space"><! -- невидимая картинка чтобы выровнять кнопки, надо исправить -->
-      <a href="?option=category&id_category=1"><img src="images/buttons/button_novosti.png" alt="news" style="height: calc(10vw/3); min-height:40px" class="buttons"></a><! -- ТУТ УКАЗАТЬ id_category из БАЗЫ ДАННЫХ -->
-      <a href="?option=category&id_category=2"><img src="images/buttons/button_galereya.png" alt="galery" style="height: calc(10vw/3); min-height:40px" class="buttons"></a>
-      <a href="?option=category&id_category=3"><img src="images/buttons/button_gajdy.png" alt="guides" style="height: calc(10vw/3); min-height:40px" class="buttons"></a>
-      <a href="?option=category&id_category=4"><img src="images/buttons/button_recepty.png" alt="recepts" style="height: calc(10vw/3); min-height:40px" class="buttons"></a>
-      <a href="?option=category&id_category=5"><img src="images/buttons/button_loshadi.png" alt="horses" style="height: calc(10vw/3); min-height:40px" class="buttons"></a>
-      <a href="?option=category&id_category=6"><img src="images/buttons/button_karty.png" alt="maps" style="height: calc(10vw/3); min-height:40px" class="buttons"></a>
-      <a href="?option=category&id_category=7"><img src="images/buttons/button_bossy.png"  alt="boss" style="height: calc(10vw/3); min-height:40px" class="buttons"></a></div>
-      </div>
-<div class="semilayer">
+<div class="content">
